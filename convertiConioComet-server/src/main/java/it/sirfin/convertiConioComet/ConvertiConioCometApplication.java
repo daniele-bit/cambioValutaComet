eb467980940ddfa34c9d5b9f1e34c2c2a4fc46f8
daniele-bit <daniele.fas95@gmail.com>
2021-05-03 14:23:03 +0200

    Librerie Spring

---
+++ convertiConioComet-server/src/main/java/it/sirfin/convertiConioComet/ConvertiConioCometApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.convertiConioComet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertiConioCometApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertiConioCometApplication.class, args);
+	}
+
+}
---
+++ convertiConioComet-server/src/test/java/it/sirfin/convertiConioComet/ConvertiConioCometApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.convertiConioComet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertiConioCometApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
